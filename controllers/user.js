d45b0f5a4af5c07ab235af8778344ad5265dad06
hlakhara <hemrajlakhara91111@gmail.com>
2025-01-26 16:19:54 +0530

    we now using jwt to store the session in the cookie as it will use state less session management. Even if the server restarts user will not be logged out.

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const user = require('../models/user');
 const {v4: uuidv4} = require('uuid');
 const {setUser,getUser} = require('../services/auth');
+const { set } = require('mongoose');
 async function handleUserRegister(req,res){
     const {name, email, password} = req.body;
     await user.create({
@@ -14,9 +15,8 @@ async function handleUserLogin(req,res){
     const {email, password} = req.body;
     const result = await user.findOne({email,password});
     if(!result) return res.status(400).render("login",{error:"Invalid email or password"});
-    const sessionId = uuidv4();
-    setUser(sessionId,result);
-    res.cookie('userid',sessionId);
+    const token = setUser(result); // this token is the sessionId that we will use to identify the user
+    res.cookie('userid',token); // we set the cookie with the sessionId
     return res.redirect('/');
 }
 module.exports = {
--- services/auth.js
+++ services/auth.js
@@ -1,10 +1,21 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(sessionId,user){
-    sessionIdToUserMap.set(sessionId,user);
+const jwt = require('jsonwebtoken');
+const secret = "My_SeCrEt_K3Y";
+//with session id if the server restarts the session will be lost as it was stored in memory
+//we can use jwt to store the session in the cookie as it will use state less session management
+function setUser(user){
+    return jwt.sign({
+        _id:user._id,
+        email:user.email
+    },secret);
 }
-function getUser(sessionId){
-    return sessionIdToUserMap.get(sessionId);
+function getUser(token){
+    if(!token) return null;
+    try{
+        return jwt.verify(token,secret);
+    }
+    catch(e){
+        return null;
+    }
 }
 
 module.exports={
